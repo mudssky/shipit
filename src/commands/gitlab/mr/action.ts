78e73d4abd87ce63e388adfd55f27c167bc70024
mudssky <mudssky@gmail.com>
2025-11-14 09:50:07 +0800

    refactor: 移除GitLab相关命令和自动化功能
    清理不再使用的GitLab命令模块和相关自动化代码
    移除对@xhgj/xh-jsutils的依赖
    简化全局配置结构

--- src/commands/gitlab/mr/action.ts
+++
@@ -1,9 +0,0 @@
-import { globalConfig } from '@/config'
-import { GitlabAutomator } from '@/playwright/gitlabAutomator'
-
-export async function autoMerge(url: string) {
-  const ga = await GitlabAutomator.createCustomWebsiteAutomator(
-    globalConfig.playwrightConfig.websiteOption.gitlab,
-  )
-  await ga.autoMerge(url)
-}
